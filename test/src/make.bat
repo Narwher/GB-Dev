612b5155fc0baff7b62b02c7e8ca5deb7eb82633
Narwher <59769041+Narwher@users.noreply.github.com>
2020-04-14 22:27:51 -0400

    rename build.bat -> make.bat
    Also modified to run binary automatically

---
+++ test/src/make.bat
@@ -0,0 +1,6 @@
+C:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -c -o ..\build\main.o main.c
+C:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -Wf-bo1 -Wf-ba1 -c -o ..\build\bank1.o bank1.c
+C:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -Wl-yt2 -Wl-yo4 -Wl-ya1 -o ..\bin\main.gb ..\build\main.o ..\build\bank1.o
+cd ..\..\emulator\
+bgb.exe ..\test\bin\main.gb
+pause
